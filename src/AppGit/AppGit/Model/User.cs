328553ac08f3f907c94fd03248f4276fe7900be8
lucasmdo <lucasmdo92@gmail.com>
2017-05-07 20:03:32 -0300

    Added relevant properties to User model.

--- src/AppGit/AppGit/Model/User.cs
+++ src/AppGit/AppGit/Model/User.cs
@@ -8,6 +8,10 @@ namespace AppGit.Model
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string AvatarUrl { get; set; }
 
     }
 }
