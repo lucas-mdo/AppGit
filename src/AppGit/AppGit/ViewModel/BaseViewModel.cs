b8a6cc247b973ba573965562d610941042949ec2
Lucas Miranda de Oliveira <luketeoli@hotmail.com>
2017-05-10 15:22:22 -0300

    Refactored BaseViewModel.
    Implemented SetProperty method to encapsulate the notify property changed process.

--- src/AppGit/AppGit/ViewModel/BaseViewModel.cs
+++ src/AppGit/AppGit/ViewModel/BaseViewModel.cs
@@ -1,10 +1,6 @@
-﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AppGit.ViewModel
 {
@@ -12,9 +8,21 @@ namespace AppGit.ViewModel
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void NotifyPropertyChanged([CallerMemberName]string propertyName = null)
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            //Ensure that new value is different from current value to propagate the change
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+                return false;
+                
+            storage = value;
+            OnPropertyChanged(propertyName);
+            
+            return true;
         }
     }
 }
