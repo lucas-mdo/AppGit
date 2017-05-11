b1ee1a97e6b5eec69f4a67d22d87970b345f64fd
lucasmdo <lucasmdo92@gmail.com>
2017-05-11 19:48:52 -0300

    Added Repositories page.

---
+++ src/AppGit/AppGit/Pages/RepositoriesPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppGit.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RepositoriesPage : ContentPage
+    {
+        public RepositoriesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
