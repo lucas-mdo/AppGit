4b61e4bb6e0edf9cb9472d49c099e43d7f436b35
lucasmdo <lucasmdo92@gmail.com>
2017-05-11 21:15:30 -0300

    Added a viewmodel to the Repository page.

---
+++ src/AppGit/AppGit/ViewModel/RepositoriesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using AppGit.Model;
+using Xamarin.Forms;
+
+namespace AppGit.ViewModel
+{
+    public class RepositoriesViewModel : BaseViewModel
+    {
+        public RepositoriesViewModel()
+        {
+            RepositoriesCollection = new ObservableCollection<Repository>();
+        }       
+
+        public ObservableCollection<Repository> RepositoriesCollection;
+
+    }
+}
